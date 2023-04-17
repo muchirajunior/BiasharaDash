552cac11aec8fdbd81796df4385bbd52a63b2649
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2023-04-18 02:50:21 +0300

    sales complete

--- controllers/orders.py
+++ controllers/orders.py
@@ -14,18 +14,43 @@ def orders_route():
         customer=request.form.get('customer')
         db.session.add(Order(customer,0,current_user.business_id))
         db.session.commit()
-    business_orders=Order.query.order_by(Order.created_at.desc()).filter_by(business_id=current_user.business_id).all()
+    business_orders=Order.query.order_by(Order.created_at.desc()).filter_by(business_id=current_user.business_id,sold=False).all()
     return render_template("orders.html",orders=business_orders)
 
 @orders.route("/<id>",methods=['POST','GET'])
 @login_required
 def order_route(id):
-    order= Order.query.filter(Order.id==id,Order.business_id==current_user.business_id).first()
+    order= Order.query.filter(Order.id==id,Order.sold ==False,Order.business_id==current_user.business_id).first()
     if order == None:
         return redirect('/orders')
+    
+    if request.method == 'POST':
+        name=request.form.get('name')
+        quantity=request.form.get('quantity')
+        price=request.form.get('price')
+        item_id=request.form.get('item_id')
+        db.session.add(OrderItem(name,price,quantity,item_id,order.id))
+        order.total+=float(price)*int(quantity)
+        db.session.commit()
     search=request.args.get('search')
     if search==None:
         search=''
-    items=Item.query.filter(Item.business_id==current_user.business_id,Item.stock>0,Item.name.like(f"%{search}%"))
-    order_items=OrderItem.query.filter_by(order_id=order_route.id).all()
+
+    items=Item.query.filter(Item.business_id==current_user.business_id,Item.active,Item.name.like(f"%{search}%")).all()
+    order_items=OrderItem.query.filter_by(order_id=order.id).all()
     return render_template("order_page.html",order=order,order_items=order_items,items=items)
+
+@orders.route("/<id>/<itemid>")
+@login_required
+def remove_item_route(id,itemid):
+    order:Order= Order.query.filter(Order.id==id,Order.sold ==False,Order.business_id==current_user.business_id).first()
+    if order == None:
+        return redirect('/orders')
+    item:OrderItem=OrderItem.query.filter_by(id=itemid).first()
+    if item ==None:
+        return redirect('/orders')
+    order.total-=item.price*item.quantity
+    db.session.delete(item)
+    db.session.commit()
+
+    return redirect(request.referrer)
--- models/order_items.py
+++ models/order_items.py
@@ -6,11 +6,13 @@ class OrderItem(db.Model):
     name=db.Column(db.String(50),nullable=False)
     price=db.Column(db.Float,nullable=False)
     quantity=db.Column(db.Integer)
+    item_id=db.Column(db.Integer)
     order_id=db.Column(db.Integer,db.ForeignKey("orders.id"),nullable=False)
 
-    def __init__(self,name,price,quantity,order_id):
+    def __init__(self,name,price,quantity,item_id,order_id):
         self.name=name
         self.price=price
         self.quantity=quantity
+        self.item_id=item_id
         self.order_id=order_id
 
