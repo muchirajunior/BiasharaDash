721e9c996917b630a639b3f91e0c530bd08c1f83
MUCHIRA JUNIOR <muchirajunior022@gmail.com>
2023-04-28 21:47:19 +0300

    :tada:

--- models/cartegory.py
+++ models/cartegory.py
@@ -5,6 +5,7 @@ class Cartegory(db.Model):
     __tablename__="cartegories"
     id=db.Column(db.Integer,primary_key=True)
     name=db.Column(db.String(50),nullable=False)
+    address_name=db.Column(db.String(50))
 
     def __init__(self,name):
         self.name=name
